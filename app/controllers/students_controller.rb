5bc7c12d811e13ec04388332cf2b673992db8cbe
martin mithi <gathitumithi9000@gmail.com>
2022-11-26 12:56:32 +0300

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,15 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    # byebug
     render json: students
   end
+
+  def show
+    student=Student.find_by(id: params[:id])
+    render json: student
+  end
+
+  
 
 end
